77d7f6698a9f85f440c185e2099ce4dba718665f
Martin Boissonneault <70227852+Martin-HiPi@users.noreply.github.com>
2022-04-28 20:34:32 -0400

    Add service stop/start
    Stop the service before updating,
    Copy/update the service script,
    Start the service!

--- scripts/install-UPS.sh
+++ scripts/install-UPS.sh
@@ -14,10 +14,17 @@ git clone https://github.com/hipi-io/ups-hat
 cd ups-hat/scripts
 
 # Make the script executable
-sudo chmod +x ups.sh
+sudo chmod -v +x ups.sh
 
-#copy the script to the init.d directory to run the script on startup
+# Stop previous instance, if any
+sudo systemctl stop ups
+
+# copy the script to the init.d directory to run the script on startup
 sudo cp -v ups.sh /etc/init.d/ups.sh
 
-#update the rc file
+# update the rc file
 sudo update-rc.d ups.sh defaults
+
+echo "Starting the service..."
+sudo systemctl start ups
+echo "Service active!"
